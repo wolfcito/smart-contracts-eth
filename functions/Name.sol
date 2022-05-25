ee81d5cfeea28ff0cde2b901107b68b6eafad3d5
Luis Fernando Ushiña <guffenix@gmail.com>
2022-05-24 21:46:20 -0500

    avoid change status variables with pure function

--- functions/Name.sol
+++ functions/Name.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Asset {
-    string name = "PlatziCoin";
+    string name = "Wolfcito";
 
     function getName() public view returns (string memory) {
         return name;
